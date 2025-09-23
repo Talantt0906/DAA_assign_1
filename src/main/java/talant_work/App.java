665652ef3bf31a49c3287eba5ae222a0abcb15d6
Talantt0906 <talante0906@gmail.com>
2025-09-23 10:06:34 +0500

    init: maven, junit5, ci, readme

---
+++ src/main/java/talant_work/App.java
@@ -0,0 +1,13 @@
+package talant_work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
