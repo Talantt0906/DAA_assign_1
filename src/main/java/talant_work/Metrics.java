2543bf683310ec39d0cf621e32b0a1aba356ebf8
Talantt0906 <talante0906@gmail.com>
2025-09-23 14:25:54 +0500

    feat(metrics): counters, depth tracker, CSV writer

---
+++ src/main/java/talant_work/Metrics.java
@@ -0,0 +1,12 @@
+package talant_work;
+
+public class Metrics{
+    public static int comparisons = 0;
+    public static int recursion_depth = 0;
+
+
+public static void reset() {
+    comparisons = 0;
+    recursion_depth = 0;
+}
+}
