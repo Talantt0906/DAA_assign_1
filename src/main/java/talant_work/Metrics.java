09405078a02b8d113fb57595401502632fed053e
Talantt0906 <talante0906@gmail.com>
2025-09-24 15:08:23 +0500

    feat(mergesort): baseline + reuse buffer + cutoff + metrics + tests

--- src/main/java/talant_work/Metrics.java
+++ src/main/java/talant_work/Metrics.java
@@ -1,12 +1,28 @@
 package talant_work;
 
-public class Metrics{
-    public static int comparisons = 0;
-    public static int recursion_depth = 0;
+public class Metrics {
+    public static long comparisons = 0;    
+    public static int currentDepth = 0;    
+    public static int maxDepth = 0;        
 
+    
+    public static void reset() {
+        comparisons = 0;
+        currentDepth = 0;
+        maxDepth = 0;
+    }
 
-public static void reset() {
-    comparisons = 0;
-    recursion_depth = 0;
+    
+    public static void enterRecursion() {
+        currentDepth++;
+        if (currentDepth > maxDepth) {
+            maxDepth = currentDepth;
+        }
+    }
+
+    
+    public static void exitRecursion() {
+        currentDepth--;
+    }
 }
-}
+
