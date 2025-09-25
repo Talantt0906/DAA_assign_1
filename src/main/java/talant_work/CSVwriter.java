aa6229c17d576f12c058cd09adee4536e6d3a808
Talantt0906 <talante0906@gmail.com>
2025-09-25 14:10:49 +0500

    bench(mergesort): added MergeSortBench and CSVWriter

--- src/main/java/talant_work/CSVwriter.java
+++ src/main/java/talant_work/CSVwriter.java
@@ -2,14 +2,27 @@ package talant_work;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class CSVwriter{
-    public static void write(String filename,int n,int comparisons, int recursion_depth){
-        try (FileWriter writer = new FileWriter(filename,true)){
-            writer.append(n + "," + comparisons+"," + recursion_depth + "\n");
+public class CSVWriter {
+    private PrintWriter writer;
 
-        } catch (IOException e) {e.printStackTrace();
-        }
-        }
-    }    
+    
+    public CSVWriter(String fileName) throws IOException {
+        writer = new PrintWriter(new FileWriter(fileName, true)); 
+    }
+
+    // 写入一行
+    public void writeRow(String... values) {
+        String line = String.join(",", values);
+        writer.println(line);
+    }
+
+    // 关闭
+    public void close() {
+        writer.close();
+    }
 }
+
+
+
